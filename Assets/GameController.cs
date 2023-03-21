4ae55b9c1f6b254a8d599bf5e4004de99cd6c3b1
Antonio Aguirre Ivorra <antoni.aguirre@gmail.com>
2023-03-21 21:51:24 +0100

    tilemap

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] int rows;
-    [SerializeField] int columns;
-    [SerializeField] GameObject surface;
+    [SerializeField] int tileColumns;
+    [SerializeField] int tileRows;
+    [SerializeField] GameObject surfaceTile;
+    [SerializeField] GameObject[,] tileMap;
+
+    private void LoadTileMap(int _cols=0, int _rows=0)
+    {
+        if ((_cols == 0) || (_rows == 0))
+        {
+            throw new System.Exception("Wrong tileMap size");
+        }
+
+        if (tileMap is not null)
+        {
+            Tile[] _tileMap = FindObjectsOfType<Tile>();
+            for (int i = 0; i < _tileMap.Length; i++)
+            {
+                Destroy(_tileMap[i].gameObject);
+            }
+
+            System.Array.Clear(tileMap, 0, tileMap.Length);
+        }
+
+        tileMap = new GameObject[_rows, _cols];
+
+        for (int i = 0; i < tileColumns; i++)
+        {
+            for (int j = 0; j < tileRows; j++)
+            {
+                GameObject surfaceCube = Instantiate(surfaceTile, new Vector3(-1.5f * j, -1.5f * i, 0.0f), new Quaternion(0, 0, 0, 0));
+                tileMap[j, i] = surfaceCube;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        LoadTileMap(tileColumns, tileRows);
+    }
 
     void Start()
     {
-        GameObject surfaceCube = Instantiate(surface, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion(0, 0, 0, 0));
-        surfaceCube.transform.localScale = new Vector3(1.0f, 1.0f, 0.1f);
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            LoadTileMap(tileColumns, tileRows);
+        }
     }
 }
